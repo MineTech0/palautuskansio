df2f0aab4b4a14dd519c6611afec4085db3245a7
MineTech0 <niilo.kurki@hotmail.fi>
2020-08-05 15:21:26 +0300

    added build

---
+++ build/precache-manifest.3de559269e88ed149fc501d418e425e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e2879989e9f2393e5304ee81a2746f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e00bb50137c7607378",
+    "url": "/static/css/main.7635ad20.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e00bb50137c7607378",
+    "url": "/static/js/main.d8c2d5aa.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
