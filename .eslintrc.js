862b3be07442557c04a855c73c8f5fa8ff306eb8
MineTech0 <niilo.kurki@hotmail.fi>
2020-08-13 15:19:21 +0300

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,15 @@ module.exports = {
   ],
   rules: {
     "linebreak-style": 0,
+    'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+          'error', 'always'
+      ],
+      'arrow-spacing': [
+          'error', { 'before': true, 'after': true }
+      ]
+    },
+    'no-console': 0
   },
 };
