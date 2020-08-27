c90148cedee6e684518828b9a74889b92b1421dd
MineTech0 <niilo.kurki@hotmail.fi>
2020-08-27 17:48:49 +0300

    osa4

--- osa4/blogilista/models/Blog.js
+++ osa4/blogilista/models/Blog.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-  title: String,
+  title: {type: String,required: true},
   author: String,
-  url: String,
-  likes: Number
+  url: {type: String, required: true},
+  likes: {type: Number, default: 0},
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
@@ -15,4 +19,4 @@ blogSchema.set('toJSON', {
     }
   })
   
-  module.exports = mongoose.model('Blog', noteSchema)
+  module.exports = mongoose.model('Blog', blogSchema)
--- osa4/blogilista/utils/logger.js
+++ osa4/blogilista/utils/logger.js
@@ -1,11 +1,13 @@
 const info = (...params) => {
+  if (process.env.NODE_ENV !== 'test') { 
     console.log(...params)
+  }
   }
   
   const error = (...params) => {
     console.error(...params)
   }
   
-  export default {
+  module.exports = {
     info, error
   }
