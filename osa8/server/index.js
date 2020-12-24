e452c8e3e2e6f150356ccf1489b67569401ac056
MineTech0 <niilo.kurki@hotmail.fi>
2020-12-24 16:35:28 +0200

    fixed me resolver

--- osa8/server/index.js
+++ osa8/server/index.js
@@ -132,7 +132,7 @@ const resolvers = {
       return author
     },
     createUser: (root, args) => {
-      const user = new User({ username: args.username })
+      const user = new User({ ...args })
 
       return user.save().catch((error) => {
         throw new UserInputError(error.message, {
